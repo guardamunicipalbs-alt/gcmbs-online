ab6889af72b50609ac90c2114e19a1f32d366c14
guardamunicipalbs-alt <guardamunicipalbs@yahoo.com>
2026-08-21 20:46:43 -0300

    fix(audit): carregar guardiao de interface de Permutas

--- js/login-security.js
+++ js/login-security.js
@@ -6,6 +6,7 @@ import './audit-pending-fixes.js?v=100062';
 import './v62-folha-ui.js?v=100062-folha01';
 import './v62-folha-legenda.js?v=100062-folha02';
 import './v62-banco-ui-fix.js?v=100062-banco01';
+import './v62-permuta-ui-fix.js?v=100062-permuta01';
 import './v62-escala-mobile-ui.js?v=100062-escala01';
 import './v62-viaturas-ui-fix.js?v=100062-viaturas01';
 
