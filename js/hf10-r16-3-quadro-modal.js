2ad2727c9b6bf8c86bca758b73f4348765335193
guardamunicipalbs-alt <guardamunicipalbs@yahoo.com>
2026-08-31 15:29:31 -0300

    HF10 R17 Banco de Horas por GCM e competência

--- js/hf10-r16-3-quadro-modal.js
+++ js/hf10-r16-3-quadro-modal.js
@@ -62,4 +62,8 @@ document.addEventListener('click',e=>{
   queueMicrotask(()=>r163Abrir(btn));
 },false);
 
+// HF10 R17: Banco de Horas do Comando por GCM + competencia.
+import('./hf10-r17-banco-horas.js?v=20260831hf10r17')
+  .catch(err=>console.warn('[GCMBS] HF10 R17 falha ao carregar Banco de Horas',err));
+
 console.info('[GCMBS] HF10 R16.3 modal do Quadro ativo');
