65328abac28965d4db73d2c4a5877e3078383f4f
guardamunicipalbs-alt <guardamunicipalbs@yahoo.com>
2026-09-08 13:28:06 -0300

    GCMBS V133: carregar Movimentacao do Comando no Online

--- js/login-security.js
+++ js/login-security.js
@@ -1,6 +1,7 @@
 import './hf10-r21d-version-guard.js?v=100110';
 import './communication-ui-v74.js?v=100076';
 import './hf11-paridade-v69.js?v=100110';
+import './gcmbs-bank-command-v133.js?v=100133';
 
 // Politica de seguranca do login GCMBS.
 // Quando "Lembrar meu acesso" estiver desmarcado, nenhum dado de login
@@ -28,7 +29,6 @@ function configureBrowserAutocomplete(){
   user.setAttribute('autocomplete',remember?'username':'off');
   pass.setAttribute('autocomplete',remember?'current-password':'new-password');
   if(!remember){
-    // Ajuda a impedir preenchimento por gerenciadores de senha de terceiros.
     user.setAttribute('data-lpignore','true');
     pass.setAttribute('data-lpignore','true');
     user.setAttribute('data-1p-ignore','true');
@@ -49,7 +49,6 @@ function clearVisibleCredentials(force=false){
 }
 
 function schedulePostAutofillClear(){
-  // Chrome/gerenciadores podem preencher alguns ms depois do carregamento.
   for(const ms of [0,60,250,700,1400])setTimeout(()=>clearVisibleCredentials(false),ms);
 }
 
