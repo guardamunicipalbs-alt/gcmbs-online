6acb2586e176ecae126e86198efb0b35e77e69a2
guardamunicipalbs-alt <guardamunicipalbs@yahoo.com>
2026-09-23 15:21:18 -0300

    chore(web): invalidar cache do provider de acesso

--- js/app-core.js
+++ js/app-core.js
@@ -1,5 +1,5 @@
 
-import {AuthenticatedProvider} from './data-provider.js?v=100085';
+import {AuthenticatedProvider} from './data-provider.js?v=100161-accessscope';
 import {MODULOS_GCMBS} from './access-catalog.js?v=100085';
 import {PRIMARY_ENTITY,DEDICATED_VIEW,canonicalModule} from './communication-contract.js?v=100085';
 import {configurarPushNativo} from './native-push.js';
